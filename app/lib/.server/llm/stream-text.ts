02b7ae976c3363bad4c6371106334ff4fc5d62d1
arolleaguekeng <arolleaguekeng@gmail.com>
2025-05-03 02:26:44 +0100

    succesfull use geemini to generate result

--- app/lib/.server/llm/stream-text.ts
+++ app/lib/.server/llm/stream-text.ts
@@ -1,8 +1,8 @@
-import { streamText as _streamText, convertToCoreMessages } from 'ai';
+import { streamText as _streamText, convertToCoreMessages, type LanguageModelV1 } from 'ai';
 import { getAPIKey } from '~/lib/.server/llm/api-key';
-import { getAnthropicModel } from '~/lib/.server/llm/model';
 import { MAX_TOKENS } from './constants';
 import { getSystemPrompt } from './prompts';
+import { getGeminiModel } from './model';
 
 interface ToolResult<Name extends string, Args, Result> {
   toolCallId: string;
@@ -22,14 +22,22 @@ export type Messages = Message[];
 export type StreamingOptions = Omit<Parameters<typeof _streamText>[0], 'model'>;
 
 export function streamText(messages: Messages, env: Env, options?: StreamingOptions) {
+  const fixedMessages = messages.map((m) => ({
+    ...m,
+    toolInvocations: m.toolInvocations?.map((tool) => ({
+      ...tool,
+      state: 'result' as const,
+    })),
+  }));
+
   return _streamText({
-    model: getAnthropicModel(getAPIKey(env)),
+    model: getGeminiModel(getAPIKey(env)) as LanguageModelV1,
     system: getSystemPrompt(),
     maxTokens: MAX_TOKENS,
     headers: {
       'anthropic-beta': 'max-tokens-3-5-sonnet-2024-07-15',
     },
-    messages: convertToCoreMessages(messages),
+    messages: convertToCoreMessages(fixedMessages),
     ...options,
   });
 }
